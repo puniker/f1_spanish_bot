86462db1edc5b9d1f256a0249268706af187d209
Iker Sastre <isastre@baturamobile.com>
2022-05-16 22:27:55 +0200

    api header testing

--- src/services/rapidApiService.ts
+++ src/services/rapidApiService.ts
@@ -8,9 +8,9 @@ module.exports = (endpoint: string) => {
             'x-rapidapi-key': process.env.RAPID_API_KEY,
             'x-rapidapi-host': 'v1.formula-1.api-sports.io',
             'Cache-Control': 'no-cache',
-            'Postman-Token': '<calculated when request is sent>',
-            'Host': '<calculated when request is sent>',
-            'User-Agent': 'PostmanRuntime/7.29.0',
+            // 'Postman-Token': '<calculated when request is sent>',
+            // 'Host': '<calculated when request is sent>',
+            // 'User-Agent': 'PostmanRuntime/7.29.0',
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive'
