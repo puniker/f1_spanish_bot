d6a42444014c1b96b6c83f53df3300a8f12d6cf1
Iker Sastre <isastre@baturamobile.com>
2022-06-08 20:35:02 +0200

    linter

--- src/commands/scheduleGp.ts
+++ src/commands/scheduleGp.ts
@@ -8,18 +8,22 @@ const { nextGpSchedule } = require('../services/gpInfoService')
 import { TelegramMsg } from '../interfaces/TelegramInterfaces'
 
 module.exports = async (msg: TelegramMsg) => {
-    let msgTemplate: string = '';
+    let msgTemplate: string = ''
     await nextGpSchedule('1st Qualifying').then((data: GpInfoInterface) => {
         msgTemplate += `${data.response[0].competition.name} \n`
-        const qualyDate = moment(new Date(data.response[0].date)).format('DD-MM-YYYY')
-        const qualyTime = moment(new Date(data.response[0].date)).format('hh:mm')
+        const qualyDate = moment(new Date(data.response[0].date)).format(
+            'DD-MM-YYYY'
+        )
+        const qualyTime = moment(new Date(data.response[0].date)).format(
+            'hh:mm'
+        )
         msgTemplate += `Clasificación: ${qualyDate} a las ${qualyTime}. \n`
     })
     await nextGpSchedule('race').then((data: GpInfoInterface) => {
         const raceDate = moment(new Date(data.response[0].date)).format(
             'DD-MM-YYYY'
         )
-        const raceTime = moment(new Date(data.response[0].date)).format('hh:mm');
+        const raceTime = moment(new Date(data.response[0].date)).format('hh:mm')
         msgTemplate += `Carrera: ${raceDate} a las ${raceTime}. \n`
     })
     Telegram.sendMessage(msg.chat.id, msgTemplate)
