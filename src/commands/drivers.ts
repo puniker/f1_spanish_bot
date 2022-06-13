e12765502c87c953e860e1afd516db338f69ddb3
Iker Sastre <isastre@baturamobile.com>
2022-06-13 22:11:03 +0200

    feat: Send image on Telegram Message

--- src/commands/drivers.ts
+++ src/commands/drivers.ts
@@ -13,21 +13,21 @@ import { readFileContent } from '../controller/storageController'
 module.exports = async (msg: TelegramMsg) => {
     await driverRanking(2022)
         .then(function (data: DriversRankingInterface) {
-            const formatRanking = data.response.map((item: DriverRankingResponse) => {
-                return { team: item.driver.name, points: item.points }
-            })
+            // const formatRanking = data.response.map((item: DriverRankingResponse) => {
+            //     return { team: item.driver.name, points: item.points }
+            // })
             const templateData: DriverRankingModel = new DriverRankingModel(data);
             readFileContent(__dirname + '/../templates/driversTemplate.html', (fileContent) => {
                 generateImage(fileContent, `${msg.chat.id}.png`, templateData).then(() => {
-                    console.log('Image created successfully.');
                     // const base64Image = convertToBase64('./sampleImage.png', 'png');
+                    Telegram.sendPhoto(msg.chat.id, `${msg.chat.id}.png`)
                 })
             });
 
-            const msgTemplate = `${formatRanking
-                .map((item) => `${item.team} - ${item.points}`)
-                .join('\n')}`
-            Telegram.sendMessage(msg.chat.id, msgTemplate)
+            // const msgTemplate = `${formatRanking
+            //     .map((item) => `${item.team} - ${item.points}`)
+            //     .join('\n')}`
+            // Telegram.sendMessage(msg.chat.id, msgTemplate)
         })
         .catch(function (error) {
             console.log(error)
