f6e58d11641227a1a5af40a95709535c2547c4a6
Iker Sastre <isastre@baturamobile.com>
2022-08-04 20:44:24 +0200

    differcence with leader

--- src/model/driverRankingModel.ts
+++ src/model/driverRankingModel.ts
@@ -4,6 +4,7 @@ import { getTemplateColor, PrintColorInterface } from "../utils/getPrintColor";
 interface driversRanking {
     name: string;
     points: number;
+    pointsToLeader: number;
     backgroundColor: string;
     textColor: string;
     teamImage?: string;
@@ -15,12 +16,14 @@ export class DriverRankingModel {
     public drivers: driversRanking[] = [];
 
     constructor(data: DriversRankingInterface) {
+        const leader: number = data.response[0].points;
         this.season = data.parameters.season;
         data.response.map((item: DriverRankingResponse) => {
             const colors: PrintColorInterface = getTemplateColor(item.team.id);
             this.drivers.push({
                 name: item.driver.name,
                 points: (item.points) ? item.points : 0,
+                pointsToLeader: (item.points) ? leader - item.points : leader,
                 backgroundColor: colors.backgroundColor,
                 textColor: colors.textColor,
             });
--- src/utils/getPrintColor.ts
+++ src/utils/getPrintColor.ts
@@ -6,59 +6,69 @@ export const getTemplateColor = (team: number) => {
     var output: PrintColorInterface;
     switch (team) {
         case 1:
+            // RedBull
             return {
                 backgroundColor: '#15185F',
                 textColor: '#ffffff',
             }
         case 2:
+            // McLaren
             return {
-                backgroundColor: '#CB0001',
+                backgroundColor: '#F18004',
                 textColor: '#ffffff',
             }
         case 3:
+            // Ferrari
             return {
                 backgroundColor: '#FF0200',
                 textColor: '#000000',
             }
         case 18:
+            //Alfa Romero
             return {
                 backgroundColor: '#990001',
                 textColor: '#ffffff',
             }
         case 5:
+            // Mercedes
             return {
                 backgroundColor: '#01A09C',
                 textColor: '#000000',
             }
         case 13:
+            // Alpine
             return {
                 backgroundColor: '#0D1C2C',
                 textColor: '#ffffff',
             }
         case 7:
+            // ALpha Tauri
             return {
                 backgroundColor: '#012947',
                 textColor: '#ffffff',
             }      
         case 12:
+            // Williams
             return {
                 backgroundColor: '#051D42',
                 textColor: '#ffffff',
             }
         case 14:
+            // Haas
             return {
                 backgroundColor: '#ffffff',
                 textColor: '#000000',
             }
-        case 10:
+        case 17:
+            // Aston Martin F1 Team
             return {
-                backgroundColor: '',
-                textColor: '',
+                backgroundColor: '#24594E',
+                textColor: '#FFFFFF',
             }
         default:
             return {
-                backgroundColor: 'red',
-                textColor: 'white',
+                backgroundColor: 'white',
+                textColor: 'black',
             }
     }
     return output;
